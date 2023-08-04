cdd4ea8d7d242cdeef25d1c18505582756f4b4db
Edison <edison.isaza@uniongr.com>
2023-08-04 08:33:58 -0500

    Creacion de UserContext

--- backEnd/UserContext.cs
+++ backEnd/UserContext.cs
@@ -3,31 +3,31 @@ using webapi.Models;
 
 namespace webapi;
 
-public class UserContext: DbContext
+public class UserContext : DbContext
 {
-    public DbSet<User> user {get;set;}
+    public DbSet<User> user { get; set; }
 
-    public UserContext(DbContextOptions<UserContext> options) :base(options) { }
+    public UserContext(DbContextOptions<UserContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         List<User> userInit = new List<User>();
-        userInit.Add(new User() {  Nombres = "Edison Esteban", Apellidos = "Isaza López", Usuario = "eisaza", Correo = "edisonisaza@gmail.com"});
-        userInit.Add(new User() {  Nombres = "Yoamin Estiven", Apellidos = "Jaramillo Munera", Usuario = "yjaramillo", Correo = "correo@gmail.com"});
+        userInit.Add(new User() { UserId = Guid.Parse("fe2de405-c38e-4c90-ac52-da0540dfb4ef"), Nombres = "Edison Esteban", Apellidos = "Isaza López", Usuario = "eisaza", Correo = "edisonisaza@gmail.com" });
+        userInit.Add(new User() { UserId = Guid.Parse("fe2de405-c38e-4c90-ac52-da0540dfb402"), Nombres = "Yoamin Estiven", Apellidos = "Jaramillo Munera", Usuario = "yjaramillo", Correo = "correo@gmail.com" });
 
 
-        modelBuilder.Entity<User>(user=> 
+        modelBuilder.Entity<User>(user =>
         {
             user.ToTable("user");
-            user.HasKey(p=> p.UserId);
+            user.HasKey(p => p.UserId);
 
-            user.Property(p=> p.Nombres).IsRequired().HasMaxLength(30);
+            user.Property(p => p.Nombres).IsRequired().HasMaxLength(30);
 
-            user.Property(p=> p.Apellidos).IsRequired().HasMaxLength(30);
+            user.Property(p => p.Apellidos).IsRequired().HasMaxLength(30);
 
-            user.Property(p=> p.Usuario).IsRequired().HasMaxLength(15);
-            
-            user.Property(p=> p.Correo).IsRequired().HasMaxLength(60);
+            user.Property(p => p.Usuario).IsRequired().HasMaxLength(15);
+
+            user.Property(p => p.Correo).IsRequired().HasMaxLength(60);
 
             user.HasData(userInit);
         });
