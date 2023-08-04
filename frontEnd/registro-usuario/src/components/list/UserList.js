cb833178b7a585c2f309558d8f21ef1dfc0ceacd
Edison <edison.isaza@uniongr.com>
2023-08-04 12:54:00 -0500

    creacion de Modal para la edicion y nuevo usuarios

--- frontEnd/registro-usuario/src/components/list/UserList.js
+++ frontEnd/registro-usuario/src/components/list/UserList.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { getUserApi, deleteUserApi } from '../../api/UserApi';
+import { getUserApi, deleteUserApi } from '../../api/UserApi'
+import NewUser from '../form/NewUser'
+import EditUser from '../form/EditUser'
+import { Modal, Row, Col, Card, ListGroup, Button } from 'react-bootstrap'
 
 export default function UserList() {
 
   const [users, setUsers] = useState([])
+  const [seEdit, setEdit] = useState(false)
+  const [seeNew, setNew] = useState(false)
+  const [userId, setUserId] = useState(0)
 
   useEffect(() => {
     async function fetchData() {
@@ -15,7 +21,6 @@ export default function UserList() {
 
   const hadleDeleteUser = async (id) => {
     async function fetchData() {
-      console.log("onclick")
       await deleteUserApi(id)
       const users = await getUserApi()
       setUsers(users)
@@ -23,23 +28,72 @@ export default function UserList() {
     fetchData()
   }
 
+  const hadleEditUser = (userId) => {
+    setUserId(userId)
+    if (seeNew) {
+      setEdit(true)
+      setNew(false)
+    } else {
+      setEdit(true)
+    }
+
+  }
+
+  const hadleNewUser = () => {
+    if (seEdit) {
+      setNew(true)
+      setEdit(false)
+    } else {
+      setNew(true)
+    }
+
+  }
+
+  const handleCloseNew = () => setNew(false);
+  const handleCloseEdit = () => setEdit(false);
+
   return (
     <div>
-      <div className='col-md-4'>
-        {users.map((item, index) => (
-          <div className="card mb-4" key={index}>
-            <div className="card-header">
-              {"Datos del usuairo " + item.usario}
+      <Row className=" justify-content-center align-items-center">
+        <Col xs={20} sm={16}>
+          <div className='text-center'>
+            <div className='col-md-4'>
+              <h2>Nuevo Usuario</h2>
+              <button className="btn btn-primary" onClick={hadleNewUser}>Nuevo</button>
+              {users.map((item, index) => (
+                <Card className='text-center' key={index}>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item> {"Datos del usuairo " + item.usuario} </ListGroup.Item>
+                    <ListGroup.Item>{item.nombres}</ListGroup.Item>
+                    <ListGroup.Item>{item.apellidos}</ListGroup.Item>
+                    <ListGroup.Item>{item.correo}</ListGroup.Item>
+                  </ListGroup>
+                  <Button className="btn btn-primary" onClick={() => hadleEditUser(item.userId)}>Editar</Button>
+                  <Button type='button' className="btn btn-danger" onClick={() => hadleDeleteUser(item.userId)}>Eliminar</Button>
+                </Card>
+              ))}
+              {/* {seeNew ? <NewUser />: null} */}
+              {/* {seEdit ? <EditUser userId={userId} /> : null} */}
+              <Modal show={seeNew} onHide={handleCloseNew}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Nuevo Usuario</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <NewUser />
+                </Modal.Body>
+              </Modal>
+              <Modal show={seEdit} onHide={handleCloseEdit}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Editar Usuario</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <EditUser userId={userId} />
+                </Modal.Body>
+              </Modal>
             </div>
-            <h5 className="card-title">{item.nombres + item.apellidos}</h5> 
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">{item.correo}</li>
-            </ul>
-            <a href="#" className="btn btn-primary">Editar</a>
-            <button type='button' className="btn btn-danger" onClick={() => { hadleDeleteUser(item.userId) }}>Eliminar</button>
           </div>
-        ))}
-    </div>
+        </Col>
+      </Row>
     </div >
   )
 }
